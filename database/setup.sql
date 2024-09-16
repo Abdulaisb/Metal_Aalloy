304b5e142f7364fa492ab4089f8f5a8c37a79e00
AbdulaiSeydi <abdulaisb@tamu.edu>
2024-09-16 14:16:16 -0500

    sql setup script

---
+++ database/setup.sql
@@ -0,0 +1,35 @@
+--Delete Database before running
+
+CREATE DATABASE alloy_info;
+USE alloy_info;
+
+CREATE TABLE alloy (
+alloy_id INT PRIMARY KEY
+);
+
+CREATE TABLE query (
+querid INT PRIMARY KEY,
+alloy_id int,
+request_type varchar(255)
+);
+
+CREATE TABLE stress_strain (
+ssid INT PRIMARY KEY,
+alloy_id int,
+request_type varchar(255),
+strain FLOAT,
+stress FLOAT,
+);
+
+CREATE TABLE properties (
+alloy_id INT,
+density varchar(255),
+elastic_modulus varchar(255),
+hardness varchar(255),
+poisson_ratio varchar(255),
+fatigue_strength varchar(255),
+yield_strength varchar(255),
+ultimate_tensile_strength varchar(255),
+metling_point varchar(255),
+electrical_reistance varchar(255)
+);
